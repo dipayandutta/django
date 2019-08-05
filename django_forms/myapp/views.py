83596818bd8b04d15b69cf7ec2e0a1b17fcb6234
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-08-05 04:43:35 -0400

    Django Forms Example

---
+++ django_forms/myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+# Create your views here.
+def contact(request):
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+            name = form.cleaned_data['name']
+            email= form.cleaned_data['email']
+            category = form.cleaned_data['category']
+            print (name)
+            print (email)
+            print (category)
+    template='form.html'
+    form = ContactForm()
+    return render(request,template,{'form':form})
+    #return HttpResponse('Contact View')
