b69d8cc12b1046c1e9698afcce124c8b001ae38a
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-09-07 19:35:12 +0530

    Paginator Code Added

--- ecommerce_second_step/shop/views.py
+++ ecommerce_second_step/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render ,get_object_or_404
 from django.http import HttpResponse
 from .models import Category,Products
+# For pagination
+from django.core.paginator import Paginator,EmptyPage,InvalidPage
 
 # Create your views here.
 def index(request):
@@ -11,16 +13,25 @@ def index(request):
 # Create Category View
 def allProdCat(request,c_slug=None):
     c_page      = None
-    products    = None
+    products_list    = None
     if c_slug!=None:
         # Display the products based on the category
         # Check if the product exists 
         c_page      = get_object_or_404(Category,slug=c_slug)
-        products    = Products.objects.filter(category=c_page,available=True)
+        products_list    = Products.objects.filter(category=c_page,available=True)
     else:
         # Display all the products
-        products    = Products.objects.all().filter(available=True)
-
+        products_list    = Products.objects.all().filter(available=True)
+    # paginator code
+    paginator = Paginator(products_list,6) # number of item in each page is 6
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except(EmptyPage,InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request,'shop/category.html',{'category':c_page,'products':products})    
 
 # Create the product view
