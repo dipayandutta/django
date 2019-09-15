cd072149ce3f7020376a637f25f9b431d583ac63
Dipayan Dutta <inbox.dipayan@gmail.com>
2019-09-15 20:24:56 +0530

    Cart Item Counter Added

--- ecommerce_third_step_cart_app/cart/context_processors.py
+++ ecommerce_third_step_cart_app/cart/context_processors.py
@@ -7,11 +7,11 @@ def counter(request):
         return {}
     else:
         try:
-            cart = Cart.objects.filter(cart_id=cart_id(request))
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
             cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 item_count += cart_item.quantity
-        except Cart.DoesNotExists:
+        except Cart.DoesNotExist:
             item_count = 0
 
     return dict(item_count= item_count)
