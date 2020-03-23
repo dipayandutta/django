bcbb14c814cfc9b004b7ce6157186e53ff239075
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-03-23 17:13:02 +0530

    Some Practice

---
+++ practice/example01/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import BlogPost
+# Create your views here.
+
+def blog_post_results(request,post_id):
+    obj             = BlogPost.objects.get(id=post_id)
+    template_name   = "blogs.html"
+    context         = {"object":obj}
+
+    return render(request,template_name,context,{"site_title":"results","title":"Display_blogs"})
---
+++ practice/example01/example01/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+
+def home_page(request):
+    title = 'This is The Custom Home Page'
+    return render(request,"home.html",{"site_title":"home","title":title})
