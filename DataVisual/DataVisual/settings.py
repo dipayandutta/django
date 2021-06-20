0996bbb36ca3a5e8e601c6ca4ce60ae05ac31a89
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-21 00:05:22 +0530

    Visualistation form Database using chart.js

--- DataVisual/DataVisual/settings.py
+++ DataVisual/DataVisual/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard',
+    'dashboard.apps.DashboardConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+CRISPY_TEMPLATE_BLOCK = 'bootstrap4'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
--- DataVisual/dashboard/views.py
+++ DataVisual/dashboard/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from .models import CountryData
+from .forms import CountryDataForm
+
 def index(request):
-	return render(request,'dashboard/index.html')
+	template = 'dashboard/index.html'
+	data = CountryData.objects.all()
+
+	if request.method == 'POST':
+		form = CountryDataForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+	else:
+		form = CountryDataForm()
+
+	context = {
+
+		'data': data,
+		'form': form,
+		'add_data': 'Add Data'
+	}
+
+	return render(request,template,context)
