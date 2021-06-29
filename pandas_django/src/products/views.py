4732c2d47324397c0bade478cd0f5ecc136efcad
Dipayan Dutta <inbox.dipayan@gmail.com>
2021-06-29 21:02:32 +0530

    Pandas DataFrame merging

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -13,9 +13,16 @@ def chart_select_view(request):
 	product_df = pd.DataFrame(Product.objects.all().values())
 	purchase_df= pd.DataFrame(Purchases.objects.all().values())
 	print(product_df)
+	product_df['product_id'] = product_df['id']
+
+	'''
+	Mergind to Dataframe
+	'''
+	df = pd.merge(purchase_df,product_df,on='product_id').drop(['id_y','date_y'],axis=1).rename({'id_x':'id','date_x':'date'},axis=1)
 	context  = {
 		'products':product_df.to_html(),
 		'purchase':purchase_df.to_html(),
+		'df' : df.to_html()
 
 	}
 	return render(request,'products/main.html',context)
